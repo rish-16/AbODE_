e436a0aa3478d5c89e66d98aee7831957fc54d4b
Rishabh Anand <mail.rishabh.anand@gmail.com>
2023-09-22 19:31:16 +0800

    Updates

--- generate_peptides.py
+++ generate_peptides.py
@@ -97,6 +97,6 @@ def generate(model, N_peptides, N_residues):
 
         print (amino_acids_ids.shape, polar_coords.shape)
 
-        peptide_utils.convert_to_mda_writer(amino_acids_ids, polar_coords, N_residues)
+        peptide_utils.convert_to_mda_writer(amino_acids_ids, polar_coords)
 
 generate(model, 1, [6])
