7224b6818751dd8f3c7f9eb832551919e1f26e76
Rishabh Anand <mail.rishabh.anand@gmail.com>
2023-09-24 01:05:44 +0800

    Updates

--- prepare_cremp.py
+++ prepare_cremp.py
@@ -60,9 +60,9 @@ def get_cremp_data(CREMP_PATH):
 
     return final_data, size_dist
 
-# if __name__ == "__main__":
-CREMP_PATH = "/data/rishabh/pickle/"
-final_data, size_dist = get_cremp_data(CREMP_PATH)
-print (len(final_data))
-pprint (final_data[:10])
-torch.save(final_data, "cremp_pyg_data_small.pt")
+if __name__ == "__main__":
+    CREMP_PATH = "/data/rishabh/pickle/"
+    final_data, size_dist = get_cremp_data(CREMP_PATH)
+    print (len(final_data))
+    pprint (final_data[:10])
+    torch.save(final_data, "cremp_pyg_data_small.pt")
