8c51f313180c202905ed315e27ce8d2c41b6f45d
Rishabh Anand <mail.rishabh.anand@gmail.com>
2023-09-23 11:19:12 +0800

    Updates

--- prepare_cremp.py
+++ prepare_cremp.py
@@ -16,7 +16,7 @@ pdb_sequences = os.listdir(CREMP_PATH)
 
 for pdb in pdb_sequences:
     fp = CREMP_PATH + pdb
-    residues_in_mol = [aa.strip("[]") for aa in pdb.replace("Sar", "MeG").split(".")["-1"]] # ignore 'pickle' at the end
+    residues_in_mol = [aa.strip("[]") for aa in pdb.replace("Sar", "MeG").split(".")[:-1]] # ignore 'pickle' at the end
     print (residues_in_mol)
     features = featurise_cremp.featurize_macrocycle_atoms_from_file(path=fp, residues_in_mol=residues_in_mol) # return_mol = False
     print (features)
