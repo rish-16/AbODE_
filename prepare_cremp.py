44939eba2e8103cd3cbb6152cba117519baf1f58
Rishabh Anand <mail.rishabh.anand@gmail.com>
2023-09-23 21:22:41 +0800

    Updates

--- prepare_cremp.py
+++ prepare_cremp.py
@@ -19,6 +19,6 @@ for pdb in pdb_sequences[1:]:
     residues_in_mol = [aa.strip("[]") for aa in pdb.replace("Sar", "MeG").split(".")[:-1]] # ignore 'pickle' at the end
     print (residues_in_mol)
     features = featurise_cremp.featurize_macrocycle_atoms_from_file(path=fp, residues_in_mol=residues_in_mol) # return_mol = False
-    print (features)
+    # print (features) 
 
     break
