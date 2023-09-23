d34e2dbac7106fcd48d94c2c5ab757172b591bff
Rishabh Anand <mail.rishabh.anand@gmail.com>
2023-09-24 00:21:00 +0800

    Updates

--- prepare_cremp.py
+++ prepare_cremp.py
@@ -26,7 +26,7 @@ def get_cremp_data(CREMP_PATH):
             size_dist.append(len(residues_in_mol))
             ohe_aa, all_conf_coords = featurise_cremp.featurize_macrocycle_atoms_from_file(path=fp, residues_in_mol=residues_in_mol) # return_mol = False
             ohe_aa = torch.tensor(ohe_aa)
-
+            print (pdb, len(all_conf_coords))
             for conf_coords in all_conf_coords:
                 coords_n, coords_ca, coords_c = conf_coords
                 combined_coords = torch.from_numpy(np.concatenate([coords_n, coords_ca, coords_c], axis=1)) # (N_res, 9)
@@ -34,8 +34,8 @@ def get_cremp_data(CREMP_PATH):
                 converted_rag_polar_coords, input_rand_coords = peptide_utils.convert_coords_to_polar(coords_n, coords_ca, coords_c)
                 final_target_features = torch.cat([ohe_aa, converted_rag_polar_coords], dim=1)
 
-                input_peptide_labels = float(1/NUM_UNIQUE_AAs) * torch.ones(len(residues_in_mol), NUM_UNIQUE_AAs) # uniform array
-                input_peptide_labels = input_peptide_labels.view(-1, NUM_UNIQUE_AAs)
+                input_peptide_labels = float(1/NUM_UNIQUE_AAs) * torch.ones(len(residues_in_mol), NUM_UNIQUE_AAs + 1) # uniform array
+                input_peptide_labels = input_peptide_labels.view(-1, NUM_UNIQUE_AAs + 1)
                 amino_index = torch.tensor([i for i in range(len(residues_in_mol))]).view(-1, 1).float()
 
                 final_input_features = torch.cat([input_peptide_labels, input_rand_coords], dim=1)
