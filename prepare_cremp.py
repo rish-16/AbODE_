236866fa5d4c50d264f9248c95278db0ad9178e2
Rishabh Anand <mail.rishabh.anand@gmail.com>
2023-09-24 00:24:02 +0800

    Updates

--- prepare_cremp.py
+++ prepare_cremp.py
@@ -28,7 +28,7 @@ def get_cremp_data(CREMP_PATH):
             size_dist.append(len(residues_in_mol))
             ohe_aa, all_conf_coords = featurise_cremp.featurize_macrocycle_atoms_from_file(path=fp, residues_in_mol=residues_in_mol) # return_mol = False
             ohe_aa = torch.tensor(ohe_aa)
-            # print (pdb, len(all_conf_coords))
+            print (pdb, len(all_conf_coords))
             for conf_coords in all_conf_coords:
                 coords_n, coords_ca, coords_c = conf_coords
                 combined_coords = torch.from_numpy(np.concatenate([coords_n, coords_ca, coords_c], axis=1)) # (N_res, 9)
