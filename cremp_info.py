51b6adab9c8a2937fb70d442095e9e04dcaf565d
rish-16 <mail.rishabh.anand@gmail.com>
2023-09-23 21:18:09 +0800

    Add cremp size dist

--- cremp_info.py
+++ cremp_info.py
@@ -12,8 +12,11 @@ for pdb in pdb_sequences[1:]:
     residues_in_mol = [aa.strip("[]") for aa in pdb.replace("Sar", "MeG").split(".")[:-1]] # ignore 'pickle' at the end
     sizes.append(len(residues_in_mol))
 
-fig = plt.figure()
-plt.hist(sizes, color="blue")    
-plt.xlabel("Macrocyclic peptide # residues", fontsize=15)
-plt.ylabel("Frequency", fontsize=15)
-plt.savefig("cremp_protein_sizes.pdf")
+# fig = plt.figure()
+# plt.hist(sizes, color="blue")    
+# plt.xlabel("Macrocyclic peptide # residues", fontsize=15)
+# plt.ylabel("Frequency", fontsize=15)
+# plt.savefig("cremp_protein_sizes.pdf")
+
+sizes = np.array(sizes)
+np.save("cremp_size_dist.npy", sizes)
