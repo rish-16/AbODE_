d44f2836c20849a90fe0896c590a75a8199a42ed
Rishabh Anand <mail.rishabh.anand@gmail.com>
2023-09-29 11:08:13 +0800

    Add edge attr

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ print ("Loaded dataset ...")
 n_instances = len(cremp_data)
 train_size = int(0.8 * n_instances)
 peptide_data_train, peptide_data_test = cremp_data[:train_size], cremp_data[train_size:][:70] # test size of 50 peptides
-train_loader = tg.loader.DataLoader(peptide_data_train, batch_size=256)
+train_loader = tg.loader.DataLoader(peptide_data_train, batch_size=300)
 test_loader = tg.loader.DataLoader(peptide_data_test, batch_size=1)
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
