93c7abccbd8185abf9e478ed563cbe750b00bd0c
Rishabh Anand <mail.rishabh.anand@gmail.com>
2023-10-02 22:06:10 +0800

    Updates

--- graph_cnf.py
+++ graph_cnf.py
@@ -12,7 +12,7 @@ from typing import *
 from zuko.utils import odeint
 
 import torch_geometric as tg
-# import gvpgnn
+import gvpgnn
 import peptide_utils
 
 class MLP(nn.Sequential):
@@ -36,14 +36,14 @@ class CNF(nn.Module):
     def __init__(
         self,
         features: int,
-        freqs: int = 3,
+        freqs: int = 200,
         **kwargs,
     ):
         super().__init__()
 
-        self.net = MLP(2 * freqs + features, features, **kwargs)
+        # self.net = MLP(2 * freqs + features, features, **kwargs)
         # gvpgnn.EGNNModel(num_layers=5, emb_dim=256, out_dim=features)
-        # self.net = EGNN(2 * freqs + features, features)
+        self.net = gvpgnn.EGNNModel(num_layers=5, in_dim=(2 * freqs) + features, emb_dim=256, out_dim=features)
 
         self.register_buffer('freqs', torch.arange(1, freqs + 1) * torch.pi)
 
@@ -79,11 +79,9 @@ class CNF(nn.Module):
 
         return Normal(0.0, z.new_tensor(1.0)).log_prob(z).sum(dim=-1) + ladj * 1e2
 
-
 class FlowMatchingLoss(nn.Module):
     def __init__(self, v: nn.Module):
         super().__init__()
-
         self.v = v
 
     def forward(self, x: Tensor, edge_index: Tensor) -> Tensor:
@@ -94,37 +92,44 @@ class FlowMatchingLoss(nn.Module):
 
         return (self.v(t.squeeze(-1), y, edge_index) - u).square().mean()
 
+cremp_data = torch.load("cremp_data_ca_only.pt")
+print ("Loaded dataset ...")
+n_instances = len(cremp_data)
+train_size = int(0.8 * n_instances)
+peptide_data_train, peptide_data_test = cremp_data[:train_size], cremp_data[train_size:][:70] # test size of 50 peptides
+train_loader = tg.loader.DataLoader(peptide_data_train, batch_size=512)
+test_loader = tg.loader.DataLoader(peptide_data_test, batch_size=1)        
 
-if __name__ == '__main__':
-    flow = CNF(2, hidden_features=[256] * 3, freqs=200)
+# if __name__ == '__main__':
+#     flow = CNF(58, hidden_features=[256] * 3, freqs=200)
 
-    # Training
-    loss = FlowMatchingLoss(flow)
-    optimizer = torch.optim.AdamW(flow.parameters(), lr=1e-3)
+#     # Training
+#     loss = FlowMatchingLoss(flow)
+#     optimizer = torch.optim.AdamW(flow.parameters(), lr=1e-3)
 
-    data, _ = make_moons(4096, noise=0.05)
-    print (data[:5])
-    print (data.shape)
-    # data = torch.from_numpy(data).float()
+#     data, _ = make_moons(4096, noise=0.05)
+#     print (data[:5])
+#     print (data.shape)
+#     # data = torch.from_numpy(data).float()
 
-    # for epoch in tqdm(range(4096), ncols=88):
-    #     subset = torch.randint(0, len(data), (256,))
-    #     x = data[subset]
+#     # for epoch in tqdm(range(4096), ncols=88):
+#     #     subset = torch.randint(0, len(data), (256,))
+#     #     x = data[subset]
 
-    #     optimizer.zero_grad()
-    #     loss(x).backward()
-    #     optimizer.step()
+#     #     optimizer.zero_grad()
+#     #     loss(x).backward()
+#     #     optimizer.step()
 
-    # # Sampling
-    # with torch.no_grad():
-    #     z = torch.randn(4096, 2)
-    #     x = flow.decode(z).numpy()
+#     # # Sampling
+#     # with torch.no_grad():
+#     #     z = torch.randn(4096, 2)
+#     #     x = flow.decode(z).numpy()
 
-    # plt.hist2d(x[:, 0], x[:, 1], bins=64)
-    # plt.savefig('moons.pdf', dpi=300)
+#     # plt.hist2d(x[:, 0], x[:, 1], bins=64)
+#     # plt.savefig('moons.pdf', dpi=300)
 
-    # # Log-likelihood
-    # with torch.no_grad():
-    #     log_p = flow.log_prob(data[:4])
+#     # # Log-likelihood
+#     # with torch.no_grad():
+#     #     log_p = flow.log_prob(data[:4])
 
-    # print(log_p)
+#     # print(log_p)
